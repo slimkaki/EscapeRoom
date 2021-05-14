a349a0a458e67a9aa441b9933a466a69a31b9e99
lucaslealvale <lucaslealvale01@gmail.com>
2021-05-13 21:01:47 -0300

    updating
    Co-authored-by: Rafael Almada <rafaalmada98@gmail.com>

---
+++ Assets/_scripts/animationState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationState : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash,isRunningHash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash  = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+        Debug.Log(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool runPressed = Input.GetKey("left shift");
+
+        // idle -> walk
+        if(!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash,true);
+        }
+        if(isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash,false);
+        }
+        
+        // walk -> run
+        if(!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash,true);
+        }
+        if(isRunning && (forwardPressed || runPressed))
+        {
+            animator.SetBool(isRunningHash,false);
+        }
+        
+    
+
+    }
+}
