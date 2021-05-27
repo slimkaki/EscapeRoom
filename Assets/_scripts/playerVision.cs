0b6c4b32aa0bfb7ff83ade9a38129c46deb80cf9
lucaslealvale <lucaslealvale01@gmail.com>
2021-05-27 17:21:42 -0300

    updating
    Co-authored-by: Rafael Almada <rafaalmada98@gmail.com>

--- Assets/_scripts/playerVision.cs
+++ Assets/_scripts/playerVision.cs
@@ -7,7 +7,7 @@ public class playerVision : MonoBehaviour {
 
     public float mouseSense = 100f;
     public Transform playerBody;
-    
+    public GameObject lanterna;
     private Camera camera;
     // [SerializeField]
     // public GameObject player;
@@ -18,6 +18,7 @@ public class playerVision : MonoBehaviour {
         Cursor.lockState = CursorLockMode.Locked;
         camera = gameObject.GetComponent<Camera>();
         // gm = GameManager.GetInstance();
+        lanterna.SetActive(false);
     }
     void Update(){
         // if(gm.gameState != GameManager.GameState.GAME) {
@@ -52,18 +53,28 @@ public class playerVision : MonoBehaviour {
 
         {
         RaycastHit hit;
-        Debug.DrawRay(camera.transform.position, transform.forward, Color.magenta);
+        Debug.DrawRay(camera.transform.position, transform.forward*4, Color.magenta);
         if(Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit, 100.0f))
         {
             
-            if(hit.collider.tag=="key" && Input.GetMouseButtonDown(0)){
+            if(hit.collider.tag=="card" && Input.GetMouseButtonDown(0)){
                 Destroy(hit.transform.gameObject);
-
-                Debug.Log(hit.collider.name);
-                
-            
+                Debug.Log($"Peguei o objeto: {hit.collider.name}");
+            }
+            if(hit.collider.tag=="paraquedas" && Input.GetMouseButtonDown(0)){
+                Destroy(hit.transform.gameObject);
+                Debug.Log($"Peguei o objeto: {hit.collider.name}");
+            }
+            if(hit.collider.tag=="tool" && Input.GetMouseButtonDown(0)){
+                Destroy(hit.transform.gameObject);
+                Debug.Log($"Peguei o objeto: {hit.collider.name}");
+            }
+            if(hit.collider.tag=="lanterna" && Input.GetMouseButtonDown(0)){
+                Destroy(hit.transform.gameObject);
+                Debug.Log($"Peguei o objeto: {hit.collider.name}");
+                lanterna.SetActive(true);
             }
         }
-        }
+    }
        
 }
