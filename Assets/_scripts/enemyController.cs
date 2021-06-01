16ac5ad8d417b030a26f1cfbbde0724015516302
lucaslealvale <lucaslealvale01@gmail.com>
2021-06-01 14:57:56 -0300

    updating
    Co-authored-by: Rafael Almada <rafaalmada98@gmail.com>

--- Assets/_scripts/enemyController.cs
+++ Assets/_scripts/enemyController.cs
@@ -1,41 +1,97 @@
 ﻿// //   enemy roamming por : dave game development disponivel em: https://youtu.be/UjkSFoLxesw
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
-// public class enemyController : MonoBehaviour {
-//     void Start()
-//     {
-        
-//     }
+public class enemyController : MonoBehaviour
+{
+    public NavMeshAgent agent;
 
-//     void Update()
-//     {
-        
-//     }
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
     
-//     private void patrulha()
-//     {
-//         if (!walkPointSet) pontosPatrulha();
 
-//         if (walkPointSet)
-//             agent.SetDestination(walkPoint);
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
 
-//         Vector3 distanceToWalkPoint = transform.position - walkPoint;
+    //Attacking
+    
+    bool alreadyAttacked;
 
-//         //Walkpoint reached
-//         if (distanceToWalkPoint.magnitude < 1f)
-//             walkPointSet = false;
-//     }
-//     private void pontosPatrulha()
-//     {
-        
-//         float randomZ = Random.Range(-walkPointRange, walkPointRange);
-//         float randomX = Random.Range(-walkPointRange, walkPointRange);
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
 
-//         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
 
-//         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-//             walkPointSet = true;
-//     }
-// }
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (!playerInSightRange && playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {   
+        Debug.Log("Enemy patrolling");
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        Debug.Log("Enemy searching");
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        Debug.Log("Enemy chasing");
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {   
+        Debug.Log("Enemy attacking");
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+   
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
